4558f6bdab3f80c4b6c180c59b17024ecfc6c1d2
Jiho Lim <very725@naver.com>
2020-01-07 08:33:46 +0900

    show random image when head_image is none

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,10 @@ class Post (models.Model):
     title = models.CharField(max_length= 30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/%Y/%m/%d', blank=True)
+    # blank = True 이 항목은 꼭 채우지 않아도 된다
+    # models.py가 수정될 때마다 migration 해줘야함
+
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
 
